b6d7d7e948769dc16daed8cdac7e463070304c84
Gustavo-Unifio <272164@unifio.edu.br>
2025-08-22 01:37:36 +0000

    Configurando o LOMBOK

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,13 @@ package app;
 
 public class PrimeiraClasse {
     public static void main(String[] args) {
+        Aluno al = new Aluno();
+        al.setNome("Kazu");
+        al.setIdade(19);
+
+        System.out.println(al.getNome());
+        System.out.println(al.getIdade());
+
         System.out.println("Olá Gradle!!!");
     }
 }
